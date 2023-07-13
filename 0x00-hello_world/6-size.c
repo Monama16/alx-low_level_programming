6025dd844bad7c5a2bccd0289a10b6900c5b4b87
Mariam <adelankeoguntade16@gmail.com>
2023-07-13 13:14:16 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - printf function
+* Return: 0
+*/
+
+int main(void)
+{
+
+/* sizeof evaluates the size of a variable */
+
+    printf("Size of char: %lu bytes\n", sizeof(char));
+    printf("Size of short: %lu bytes\n", sizeof(short));
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of long: %lu bytes\n", sizeof(long));
+    printf("Size of float: %lu bytes\n", sizeof(float));
+
+return (0)
+}
+
