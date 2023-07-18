bff3797757d2dbca88a482e3c6a452e5cb58eea9
Mariam <adelankeoguntade16@gmail.com>
2023-07-18 04:26:57 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main -  Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
