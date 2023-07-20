e1b8a9f9d1eb99d27bf340c0fbdbe4a308c4324a
Monama <adelankeoguntade16@gmail.com>
2023-07-20 16:00:48 +0100

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,23 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0 i % 5 != 0)
 		{
 			printf(" Fizz");
-		} else if (i % 5 == 0 && i % 3 != 0)
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
-		} else if (i == 1)
+		}
+		else if (i == 1)
 		{
 			printf(" %d", i);
-		} else
+		}
+		else
 		{
 			printf(" %d", i);
 		}
