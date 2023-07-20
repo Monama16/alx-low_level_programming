0af396e61c404ae830ff019dcf002d0a841779e2
Monama <adelankeoguntade16@gmail.com>
2023-07-20 15:12:20 +0100

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,11 @@
  */
 void print_numbers(void)
 {
-        int i;
+        char c;
 
-	for (i = 0; i < 10; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i + '0');
+	_putchar(c);
 	}
-
 	_putchar('\n');
 }
