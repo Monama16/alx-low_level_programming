7fb2ee05c61c031b73ddecb3fe4c3ab6814f0881
Monama <adelankeoguntade16@gmail.com>
2023-07-20 16:05:33 +0100

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,7 @@
  * main - prints the numbers from 1 to 100, followed by a new line
  * but for multiples of three prints Fizz instead of the number
  * and for the multiples of five prints Buzz
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -30,7 +30,9 @@ int main(void)
 			printf(" %d", i);
 		}
 	}
+
 	printf("\n");
+
 	return (0);
 }
 	
