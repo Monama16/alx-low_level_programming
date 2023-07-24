73a73311de1d481b648083deaf75055764785e09
Monama <adelankeoguntade16@gmail.com>
2023-07-25 00:42:29 +0100

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *  _puts - prints a string, followed by a new line, to stdout
+ *  @str: string
+ *  Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		{
+			putchar(str[i]);
+			i++
+		}
+	putchar ('\n');
+}
+
